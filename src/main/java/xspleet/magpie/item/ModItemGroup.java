52d654846e21dbbe373ac6f9cc76c76cbb1dd3ff
Kiryl Yalchik <kiryl.yalchik@mailbox.tu-dresden.de>
2024-06-16 19:24:21 +0200

    fix. fixes for 1.20.2

--- src/main/java/xspleet/magpie/item/ModItemGroup.java
+++ src/main/java/xspleet/magpie/item/ModItemGroup.java
@@ -9,12 +9,56 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import xspleet.magpie.MagpieMod;
 
+import static xspleet.magpie.item.ModItems.*;
+
 public class ModItemGroup
 {
-    public static final ItemGroup MAGPIE = Registry.register(Registries.ITEM_GROUP, new Identifier(MagpieMod.MOD_ID,"magpie"),
-            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.magpie"))
-                    .icon(() -> new ItemStack(ModItems.FORK))
-                    .entries(((displayContext, entries) -> {
-                        ModItems.addItemsToEntries(entries);
-                    })).build());
+
+    public static void registerItemGroup()
+    {
+        Registry.register(Registries.ITEM_GROUP, new Identifier(MagpieMod.MOD_ID,"magpie"),
+                FabricItemGroup.builder()
+                        .displayName(Text.translatable("itemgroup.magpie"))
+                        .icon(() -> new ItemStack(FORK))
+                        .entries((displayContext, entries) -> {
+                            entries.add(FORK);
+                            entries.add(LIGHT_BULB);
+                            entries.add(RITUAL_CANDLE);
+                            entries.add(ROTTEN_TOOTH);
+                            entries.add(TRANQ_DART);
+                            entries.add(BAG_OF_DUST);
+                            entries.add(PLASTIC_SPOON);
+                            entries.add(WAX_WING);
+                            entries.add(AQUA);
+                            entries.add(AER);
+                            entries.add(TERRA);
+                            entries.add(IGNIS);
+                            entries.add(POTTED_CACTUS);
+                            entries.add(YANG);
+                            entries.add(YIN);
+                            entries.add(GHOST_IN_A_JAR);
+                            entries.add(LIZARDS_TAIL);
+                            entries.add(AETHER);
+                            entries.add(LUCKY_PEBBLE);
+                            entries.add(LUCKY_STONE);
+                            entries.add(LUCKY_ROCK);
+                            entries.add(LUCKY_BOULDER);
+                            entries.add(LIGHTNING_ARRESTER);
+                            entries.add(MELATONIN_PILL);
+                            entries.add(NERD_GLASSES);
+                            entries.add(RAIN_MARACAS);
+                            entries.add(VAMPIRE_FANG);
+                            entries.add(UMBRELLA);
+                            entries.add(POCKET);
+                            entries.add(JUPITERS_THUNDERBOLT);
+                            entries.add(URANUS_BLOOD);
+                            entries.add(SELENES_TORCH);
+                            entries.add(MARS_SPEAR);
+                            entries.add(WASP_NEST);
+                            entries.add(SALE_TAG);
+                            entries.add(HOLY_GRAIL);
+                        })
+                        .build()
+        );
+    }
 }
